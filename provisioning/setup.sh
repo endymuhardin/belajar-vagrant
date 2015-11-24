6a16661b695778d017e3b51b18a7f08c0fe0d731
Endy Muhardin <endy.muhardin@gmail.com>
2015-11-24 16:04:50 +0700

    install codeception

--- provisioning/setup.sh
+++ provisioning/setup.sh
@@ -20,6 +20,14 @@ add-apt-repository -y ppa:nginx/stable
 apt-get update
 apt-get install -y nginx
 
+echo "Konfigurasi Virtual Host Nginx"
+mkdir /home/vagrant/aplikasisaya
+cp /vagrant/provisioning/config/coba.php /home/vagrant/aplikasisaya/
+cp /vagrant/provisioning/config/nginx-vhost /etc/nginx/sites-available/aplikasisaya
+ln -s /etc/nginx/sites-available/aplikasisaya /etc/nginx/sites-enabled/
+rm /etc/nginx/sites-enabled/default
+service nginx restart
+
 echo "Instal HHVM dari PPA"
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
 add-apt-repository "deb http://dl.hhvm.com/ubuntu $(lsb_release -sc) main"
@@ -27,8 +35,6 @@ apt-get update
 apt-get install -y hhvm
 /usr/share/hhvm/install_fastcgi.sh
 /usr/bin/update-alternatives --install /usr/bin/php php /usr/bin/hhvm 60
-
-cp /vagrant/provisioning/config/coba.php /var/www/html/
 
 echo "Setup MySQL"
 apt-get install -y debconf-utils
@@ -41,6 +47,10 @@ apt-get install -y mysql-server
 echo "Install Composer"
 curl -sS https://getcomposer.org/installer | php
 mv /home/vagrant/composer.phar /usr/local/bin/composer
+
+echo "Install Codeception"
+curl -LsS http://codeception.com/codecept.phar -o /usr/local/bin/codecept
+chmod a+x /usr/local/bin/codecept
 
 echo "Install Laravel"
 rm -rf aplikasi
